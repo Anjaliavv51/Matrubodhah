1c2e0d425e4d995d4a21aae72e6e5fa606433142
Vaibhav Babele <vaibhavbabele15@gmail.com>
2025-02-02 14:27:52 +0530

    add dark mode in index page

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const mode=document.getElementById("mode");
 // Add this to your existing JavaScript file
 document.addEventListener("DOMContentLoaded", function () {
   const toggleMenu = document.querySelector(".toggle-menu");
   const navLinks = document.querySelector(".nav-links");
   const dropdowns = document.querySelectorAll(".dropdown");
 
+  //dark mode 
+  mode.onclick=function(){
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")){
+        mode.src="images/sun.png";
+    }else{
+        mode.src="images/moon.png ";
+    }
+}
   // Toggle mobile menu
   toggleMenu.addEventListener("click", function () {
     navLinks.classList.toggle("active");
